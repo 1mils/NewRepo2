64c92fe60bd0b2de4a581648931ad6fa2ea446f9
1mils <161162792+1mils@users.noreply.github.com>
2024-03-27 17:50:32 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,13 @@
+#include <iostream>
+int factorial(int a) {
+    int f = 1;
+    for (int i = 1; i < a + 1; i++) {
+        f *= i;
+    }
+    return f;
+}
+int main() {
+    int a;
+    std::cin >> a;
+    std::cout << factorial(a);
+}
